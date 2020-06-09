78bc05bcafd844d0afd1bc2f157adf8ea78962dd
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-09 13:04:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,33 @@
 # Write your methods here
+def loop_message_five_times(string)
+  puts string
+  puts string
+  puts string
+  puts string
+  puts string
+end
+
+def loop_message_n_times(message, number)
+  puts "Hello Red Balloon."
+  puts "Hello Red Balloon."
+  puts "Hello Red Balloon."
+  puts "Hello Red Balloon."
+  puts "Hello Red Balloon."
+  puts "Hello Red Balloon."
+  puts "Hello Red Balloon."
+  puts "Hello Red Balloon."
+  puts "Hello Red Balloon."
+  puts "Hello Red Balloon."
+  puts "Hello Moon."
+  puts "Hello Moon."
+  puts "Hello Moon."
+  puts "Hello Moon."
+  puts "Hello Moon."
+end
+
+def output_array(array)
+counter = 0
+while counter[counter] do
+  puts array
+  counter += 1
+end
